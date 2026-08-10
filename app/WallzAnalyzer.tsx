d973c74b346f551898037f0ecb28e44be38bd6fa
souwuk <souwuk@gmail.com>
2026-08-09 23:58:22 -0700

    Extend engine thinking time

--- app/WallzAnalyzer.tsx
+++ app/WallzAnalyzer.tsx
@@ -234,7 +234,7 @@ export default function WallzAnalyzer() {
           </div>
           <div className="engine-controls">
             <label className="control-label" htmlFor="think-time">thinking time <b>{timeMs < 1000 ? `${timeMs} ms` : `${(timeMs / 1000).toFixed(1)} s`}</b></label>
-            <input id="think-time" className="range" type="range" min="250" max="5000" step="250" value={timeMs} onChange={(e) => setTimeMs(Number(e.target.value))} />
+            <input id="think-time" className="range" type="range" min="250" max="30000" step="250" value={timeMs} onChange={(e) => setTimeMs(Number(e.target.value))} />
             <label className="control-label" htmlFor="max-depth">depth <b>{maxDepth} ply</b></label>
             <input id="max-depth" className="range" type="range" min="2" max="12" value={maxDepth} onChange={(e) => setMaxDepth(Number(e.target.value))} />
           </div>
